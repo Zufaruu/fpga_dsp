df9a08ef1d159a0830dd3a08424110130374347a
Zufaruu <zufarbagas28@gmail.com>
2023-03-23 17:01:09 +0700

    fixed array uncompatible error

--- radar_dsp/radar_dsp.runs/synth_1/fft.tcl
+++ radar_dsp/radar_dsp.runs/synth_1/fft.tcl
@@ -96,6 +96,8 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
 set_param ips.enableIPCacheLiteLoad 1
+
+read_checkpoint -auto_incremental -incremental G:/Xilinx_2022/Vivado/2022.1/proyek/radar_dsp/radar_dsp.srcs/utils_1/imports/synth_1/fft.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
--- radar_dsp/radar_dsp.srcs/sources_1/new/fft.v
+++ radar_dsp/radar_dsp.srcs/sources_1/new/fft.v
@@ -19,65 +19,76 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 `define PI 3.14159265359
-parameter N = 8;
 
-module fft(
+module fft #(
+    parameter N = 8,
+    parameter bit = 32,
+    parameter total_serial = bit*N //512
+    )(
     input clk,
-    input real in_re [0:N-1],
-    input real in_im [0:N-1],
-    output real out_re [0:N-1],
-    output real out_im [0:N-1]
+    input [bit*N-1:0] in_re,
+    input [bit*N-1:0] in_im,
+    output reg[bit*N-1:0] out_re,
+    output reg[bit*N-1:0] out_im
     );
+    reg[31:0] in_re_arr[N-1:0], in_im_arr[N-1:0], out_re_arr[N-1:0], out_im_arr[N-1:0];
+    integer i, j;
     
-    function automatic real[0:N-1][0:1] cooleytukey(real[0:N-1] in_re, real[0:N-1] in_im);
-        real out[0:N-1][0:1],
-        out_odd[0:(N/2)-1][0:1], out_even[0:(N/2)-1][0:1],
-        re_odd[0:(N/2)-1], im_odd[0:(N/2)-1],
-        re_even[0:(N/2)-1], im_even[0:(N/2)-1],
-        t_re, t_im,
-        temp_re, temp_im;
-        integer i;
-    
+    always@(posedge clk)
+        begin
         
-        // real out_im_odd[0:(N/2)-1];
-        // real out_im_even[0:(N/2)-1];
-        // real out_re_odd[0:(N/2)-1];
-        // real out_re_even[0:(N/2)-1];
+        end
+      
+    function automatic [total_serial*2-1:0] cooleytukey;
+        input [bit*N-1:0] in_re;
+        input [bit*N-1:0] in_im;
+                
+        begin
         
+        // serial to N channel I-Q
+        for(i = 0;i < N;i = i+1) // i+1 = 8
+            begin
+            for(j = 0;j < bit; j = j+1) // j+1 = 32
+                begin
+                 in_re_arr[j][i] = in_re[total_serial*2 - (bit*i+j+1)]; //511-480 
+                 in_im_arr[j][i] = in_im[total_serial - (bit*i+j+1)];
+                end
+            end 
+            
+        // cooley-tukey stato
         if (N == 1) 
             begin
-            cooleytukey [0:N-1][0] = in_re;
-            cooleytukey [0:N-1][1] = in_im;
+            cooleytukey[total_serial*2-1:0] = {in_re, in_im};
             end
-        else
-            begin
-            for(i = 0; i < (N/2)-1; i = i + 1)
-                begin
-                re_odd[i] = in_re[2*i];
-                im_odd[i] = in_im[2*i];
-                re_even[i] = in_re[2*i];
-                im_even[i] = in_im[2*i];
-                end   
+//        else
+//            begin
+//            for(i = 0; i < (N/2)-1; i = i + 1)
+//                begin
+//                for(j = bit-1; j > 0; j = j - 1)
+//                    begin
+//                    re_odd[i] = in_re[2*i];
+//                    im_odd[i] = in_im[2*i];
+//                    re_even[i] = in_re[2*i];
+//                    im_even[i] = in_im[2*i];
+//                    end
+//                end   
     
-            out_odd = cooleytukey(re_odd, im_odd);
-            out_even = cooleytukey(re_even, im_even);
+////            out_odd = cooleytukey(re_odd, im_odd);
+////            out_even = cooleytukey(re_even, im_even);
     
-            for(i = 0; i < (N/2)-1; i = i + 1) 
-                begin
-                t_re = $cos(2.0*PI*i/N);
-                t_im = -$sin(2.0*PI*i/N); 
-                temp_re = ((t_re * out_odd[i][0]) - (t_im * out_odd[i][1]));
-                temp_im = ((t_re * out_odd[i][1]) + (t_im * out_odd[i][0]));
-                cooleytukey[i][0] = out_even[i][0] + temp_re;
-                cooleytukey[i][1] = out_even[i][1] + temp_im;
-                cooleytukey[i + N/2][0] = out_even[i][0] - temp_re;
-                cooleytukey[i + N/2][1] = out_even[i][1] - temp_im;
-                end
-            end
+////            for(i = 0; i < (N/2)-1; i = i + 1) 
+////                begin
+////                t_re = $cos(2.0*PI*i/N);
+////                t_im = -$sin(2.0*PI*i/N); 
+////                temp_re = ((t_re * out_odd[i][0]) - (t_im * out_odd[i][1]));
+////                temp_im = ((t_re * out_odd[i][1]) + (t_im * out_odd[i][0]));
+////                cooleytukey[31:0][i][0] = out_even[i][0] + temp_re;
+////                cooleytukey[31:0][i][1] = out_even[i][1] + temp_im;
+////                cooleytukey[31:0][i + N/2][0] = out_even[i][0] - temp_re;
+////                cooleytukey[31:0][i + N/2][1] = out_even[i][1] - temp_im;
+////                end
+//            end 
+        end
     endfunction
-    
-    real X[0:N-1][0:1] = cooleytukey(in_re, in_im);
-    assign out_re = X[0:N-1][0];
-    assign out_im = X[0:N-1][1];
     
 endmodule
